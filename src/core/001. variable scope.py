c5da90c3e867342db607b33e8579a5af9deb7328
atifahmeda <valiantatif@gmail.com>
2026-06-24 10:19:04 +0530

    Added src folder and scope examples

---
+++ src/core/001. variable scope.py
@@ -0,0 +1,22 @@
+'''
+Concepts Learned
+--------------------------------------------------------------------------------------
+Local Scope (L): 
+    Variables defined inside a function. They are accessible only within that specific function.
+
+Enclosing Scope (E): 
+    Variables in the local scope of any enclosing functions (relevant when using nested functions). 
+    A nested function can access variables from its outer (enclosing) function.
+
+Global Scope (G): 
+    Variables defined at the top level of a script or module. 
+    They are accessible throughout the entire file.
+
+Built-in Scope (B): 
+    Names that are pre-assigned in Python, such as print, len, or Exception.
+
+'''
+
+
+
+
